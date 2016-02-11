2d6d80016555573f2482d73c5f81da96f14c9099
solsu01 <solsu01@ca.com>
2016-02-10 19:29:31 -0500

    Add directory structure and main

---
+++ PassRepo/GeneralAppSetup/src/PassRepoMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by solsu01 on 2/10/2016.
+ */
+public class PassRepoMain {
+    public static void main (String[] args) {
+        System.out.println("Dodo's Conundrum");
+        return;
+    }
+}
--- PassRepo/src/UI/Main.java
+++
@@ -1,7 +0,0 @@
-package UI;
-
-/**
- * Created by solsu01 on 2/10/2016.
- */
-public class Main {
-}
