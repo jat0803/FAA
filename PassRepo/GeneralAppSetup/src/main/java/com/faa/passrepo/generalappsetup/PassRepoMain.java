c799232c46b7027bf99b5c175169009be48d1ae2
solsu01 <solsu01@ca.com>
2016-02-14 20:28:19 -0500

    build

--- PassRepo/GeneralAppSetup/src/main/java/com/faa/passrepo/generalappsetup/PassRepoMain.java
+++ PassRepo/GeneralAppSetup/src/main/java/com/faa/passrepo/generalappsetup/PassRepoMain.java
@@ -1,4 +1,7 @@
 package com.faa.passrepo.generalappsetup;
+
+import com.faa.passrepo.ui.Login;
+
 /**
  * Created by solsu01 on 2/10/2016.
  */
@@ -6,6 +9,16 @@ public class PassRepoMain {
     public static void main (String[] args) {
         System.out.println("Dodo's Conundrum");
         System.out.println("Something significant...it just is.");
+
+        String test = Login.giveMeAString();
+
+        System.out.println("test string from Login class: " + test);
+
         return;
+    }
+
+    public static String test () {
+        String answer = "Hi I'm PassRepoMain.test";
+        return answer;
     }
 }
--- PassRepo/UI/src/main/java/com/faa/passrepo/ui/Login.java
+++ PassRepo/UI/src/main/java/com/faa/passrepo/ui/Login.java
@@ -1,6 +1,11 @@
 package com.faa.passrepo.ui;
+
 /**
  * Created by solsu01 on 2/10/2016.
  */
 public class Login {
+    public static String giveMeAString (){
+        String answer = "Here you go.";
+        return answer;
+    }
 }
